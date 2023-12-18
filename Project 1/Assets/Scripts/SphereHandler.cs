d0a3814cdd3c1d924977dff279cb40732fda8607
DESKTOP-B2U2C0N\Dimitris Filippou <dimfilip@zohomail.eu>
2023-12-18 18:43:14 +0200

    Interaction and winning condition

---
+++ Project 1/Assets/Scripts/SphereHandler.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class SphereHandler : MonoBehaviour
+{
+    public Transform player;
+
+    [SerializeField] private Color colorAt0;
+    [SerializeField] private Color colorAt1;
+
+    private Material material;
+    private ParticleSystem pS;
+
+    private void Awake()
+    {
+        MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
+        material = meshRenderer.material;
+
+        pS = GetComponentInChildren<ParticleSystem>();
+    }
+
+    [SerializeField] private float playIfProximityOver = 0.8f;
+    [SerializeField] private float stopIfProximityUnder = 1;
+
+    private float minDistance = 1.7f;
+    private float maxDistance = 10;
+
+    private void Update()
+    {
+        float distance = Vector3.Distance(transform.position, player.position);
+        float distance01 = (distance - minDistance) / (maxDistance - minDistance);
+        float proximity01 = 1 - distance01;
+
+        // Debug.Log($"Sphere is {distance} units from player {distance01}");
+
+        // Material color lerp
+        Color newColor = Color.Lerp(colorAt0, colorAt1, proximity01);
+        material.color = newColor;
+
+        // VFX On-off
+        Debug.Log($"Proximity is {proximity01}");
+
+        if (!pS.isPlaying && proximity01 > playIfProximityOver)
+        {
+            Debug.Log("PLAY!");
+            pS.gameObject.SetActive(true);
+            pS.Play();
+        }
+        else if (pS.isPlaying && proximity01 <= stopIfProximityUnder) // if (proximity01 > stopIfProximityOver)
+        {
+            Debug.Log("STOP!");
+            pS.Stop();
+            pS.gameObject.SetActive(false);
+        }
+
+        // TODO FOR HOME!
+        // SFX Change Pitch as you approach
+        // 0. Make sure sphere has an audio source & audio clip! (download one if needed) 
+        // 1. Get a reference to the audio source
+        // 2. Map the proximity to the audio source's pitch
+        // (expose any variables you need in the prefab to get the result you want)
+    }
+}
